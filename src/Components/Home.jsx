ebfbe537073831dc87c948cb52d5f3195ce4a074
DatDuong1911 <71874193+DatDuong1911@users.noreply.github.com>
2023-06-30 17:03:49 +0700

    update at 05:00 30/06/2023

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -32,28 +32,28 @@ export default function Home() {
 
 
     function deleteTask(id){
-    // let token = JSON.parse(localStorage.getItem('user'))
+    let token = JSON.parse(localStorage.getItem('user'))
 
-    //     let config = {
-    //         method: 'delete',
-    //         maxBodyLength: Infinity,
-    //         url: `https://backoffice.nodemy.vn/api/tasks/${id}`,
-    //         headers: {
-    //             'Authorization': `Bearer ${token.jwt}    ` ,
-    //             'Content-Type': 'application/json',
-    //         }
-    //     };
+        let config = {
+            method: 'delete',
+            maxBodyLength: Infinity,
+            url: `https://backoffice.nodemy.vn/api/tasks/${id}`,
+            headers: {
+                'Authorization': `Bearer ${token.jwt}    ` ,
+                'Content-Type': 'application/json',
+            }
+        };
 
-    //     axios.request(config)
-    //         .then((response) => {
-    //             console.log(JSON.stringify(response.data));
-    //             // let txtData = JSON.stringify(response.data)
-    //             // localStorage.getItem('user', txtData)
-    //             // window.location.reload();
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+                // let txtData = JSON.stringify(response.data)
+                // localStorage.getItem('user', txtData)
+                // window.location.reload();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
     return (
         <div>
