2b3104809c12e15857e00d4d6471e53d59d008e2
DatDuong1911 <71874193+DatDuong1911@users.noreply.github.com>
2023-06-30 15:23:28 +0700

    update at 03:30 30/06/2023

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,8 +3,7 @@ import { Pagination } from 'antd';
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 export default function Home() {
-    let nav =useNavigate()
-    
+    let nav = useNavigate()
     let [task, setTask] = useState([])
     const [pageInfo, setPageInfo] = useState({
         total: 9,
@@ -30,6 +29,32 @@ export default function Home() {
         localStorage.removeItem('user')
         nav("/")
     }
+
+
+    function deleteTask(id){
+    // let token = JSON.parse(localStorage.getItem('user'))
+
+    //     let config = {
+    //         method: 'delete',
+    //         maxBodyLength: Infinity,
+    //         url: `https://backoffice.nodemy.vn/api/tasks/${id}`,
+    //         headers: {
+    //             'Authorization': `Bearer ${token.jwt}    ` ,
+    //             'Content-Type': 'application/json',
+    //         }
+    //     };
+
+    //     axios.request(config)
+    //         .then((response) => {
+    //             console.log(JSON.stringify(response.data));
+    //             // let txtData = JSON.stringify(response.data)
+    //             // localStorage.getItem('user', txtData)
+    //             // window.location.reload();
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    }
     return (
         <div>
             <Pagination simple defaultCurrent={1} total={pageInfo.total} page={pageInfo.page} pageSize={pageInfo.pageSize}
@@ -44,9 +69,10 @@ export default function Home() {
             {task.map(item => {
                 return <h5 key={item.id}>
                     Task {item.id}: {item?.attributes?.title}
-                    <button onClick={()=>{
+                    <button onClick={() => {
                         nav(`/${item.id}`)
                     }}>Chi tiet</button>
+                    <button onClick={deleteTask(item.id)}>Xoa</button>
                 </h5>
             })}
             <button onClick={logOut}>
